faab793f54af50680599bc5054b5f97cc132b8c3
p1612068 <test@iut.fr>
2017-06-29 14:54:49 +0200

    Test Opération + réaliser des premières opération Calculator.cs

--- SolidExercices.Tests/CalculatorShould.cs
+++ SolidExercices.Tests/CalculatorShould.cs
@@ -10,7 +10,28 @@ namespace SolidExercices.Tests
         {
             var calculator = new Calculator();
             var result = calculator.Calculate("1+2,3");
-            Check.That(result).IsEqualTo(3.3);
+            Check.That(result).IsEqualTo(3.3m);
+        }
+
+        public void CalculateASub()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("4-2");
+            Check.That(result).IsEqualTo(2m);
+        }
+
+        public void CalculateAProd()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("2*4");
+            Check.That(result).IsEqualTo(8m);
+        }
+
+        public void CalculateADiv()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("6/3");
+            Check.That(result).IsEqualTo(2m);
         }
     }
 }
--- SolidExercices/Calculator.cs
+++ SolidExercices/Calculator.cs
@@ -5,9 +5,72 @@ namespace SolidExercices
 {
     public class Calculator
     {
-        public double Calculate(string operation)
+        public decimal Calculate(string operation)
         {
-            throw new NotImplementedException();
+            decimal resultat = 0;
+
+            if (operation.Contains('+'))
+            {
+                string[] nombres = operation.Split('+');
+
+                resultat = Convert.ToDecimal(nombres[0]);
+
+                foreach (var nombre in nombres.Skip(1).ToArray())
+                {
+                    resultat += Convert.ToDecimal(nombre);
+                }
+
+                Console.WriteLine("Opération valide");
+                return resultat;
+            }
+            else if (operation.Contains('*'))
+            {
+                string[] nombres = operation.Split('*');
+
+                resultat = Convert.ToDecimal(nombres[0]);
+
+                foreach (var nombre in nombres.Skip(1).ToArray())
+                {
+                    resultat += Convert.ToDecimal(nombre);
+                }
+
+                Console.WriteLine("Opération valide");
+                return resultat;
+            }
+            else if (operation.Contains('/'))
+            {
+                string[] nombres = operation.Split('/');
+
+                resultat = Convert.ToDecimal(nombres[0]);
+
+                foreach (var nombre in nombres.Skip(1).ToArray())
+                {
+                    resultat += Convert.ToDecimal(nombre);
+                }
+
+                Console.WriteLine("Opération valide");
+                return resultat;
+            }
+            else if (operation.Contains('-'))
+            {
+                string[] nombres = operation.Split('-');
+
+                resultat = Convert.ToDecimal(nombres[0]);
+
+                foreach (var nombre in nombres.Skip(1).ToArray())
+                {
+                    resultat += Convert.ToDecimal(nombre);
+                }
+
+                Console.WriteLine("Opération valide");
+                return resultat;
+            }
+            else
+            {
+                Console.WriteLine("Opération non valide");
+                return 0;
+            }
+            return resultat;
         }
     }
 }
